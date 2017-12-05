70926e8e572530cc808cc6221aea383ed9809102
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-05 19:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function getFirstSelector(selector){
 function nestedTarget(selector){
   return document.getElementById('nested').querySelector('.target')
 }
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div div div div')
+}
