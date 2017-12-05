d917f9d2662e4b17f7ccf9645de414f42bd0d8c4
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-05 19:53:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector)
+}
+
+function nestedTarget(selector){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+    for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = (i + n)
+  }
+
+
+
+}
