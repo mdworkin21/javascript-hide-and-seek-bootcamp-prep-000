2feea785ae4a9a88a3ea536f953cee150f5d1e8f
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-05 19:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(selector){
-  return document.querySelector(selector '#nested.target')
+  return document.querySelector(selector#nested.target)
 }
