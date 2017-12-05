d07b0762d3d98caebb18462613bedb114aa5e75b
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-05 19:42:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
-  return
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  const firstList = rankedLists[0]
+  const secondList = rankedLists[1]
+
+  let children = firstList.children
+  let start = 1
+  for (let i = 0, l = children.length; i < l; i++) {
+    parseInt()
 
 }
